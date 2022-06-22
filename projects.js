68dfeb213a05a96c05c352721d9c71861ae7c154
Anne Göbels <annegoebels95@gmail.com>
2022-06-22 12:34:16 +0200

    changed url to filepaths

--- projects.js
+++ projects.js
@@ -2,51 +2,27 @@ export const projects = [
     {
         name: "Model 1 A",
         id: "1",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/"
+        url: "ifc_files\\01.ifc"
     },
     {
         name: "Model 2",
         id: "2",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
+        url: "ifc_files\\02.ifc"
     },
     {
         name: "Model 3",
         id: "3",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
+        url: "ifc_files\\03.ifc"
     },
     {
         name: "Model 4",
         id: "4",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/"
+        url: "ifc_files\\04.ifc"
     },
     {
         name: "Model 5",
         id: "5",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/"
+        url: "ifc_files\\05.ifc"
     },
-    {
-        name: "Model 1 A",
-        id: "1",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/01/"
-    },
-    {
-        name: "Model 2",
-        id: "2",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/02/"
-    },
-    {
-        name: "Model 3",
-        id: "3",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/03/"
-    },
-    {
-        name: "Model 4",
-        id: "4",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/04/"
-    },
-    {
-        name: "Model 5",
-        id: "5",
-        url: "https://ifcjs.github.io/ifcjs-crash-course/sample-apps/05/"
-    }
+   
 ]
