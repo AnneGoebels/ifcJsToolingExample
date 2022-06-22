ba865aa58a56406d9f755d4cb3b14ff9247b0c4d
Anne Göbels <annegoebels95@gmail.com>
2022-06-22 14:19:53 +0200

    changed filepath

--- projects.js
+++ projects.js
@@ -1,28 +1,28 @@
 export const projects = [
     {
-        name: "Model 1 A",
+        name: "Model 1",
         id: "1",
-        url: "ifc_files\\01.ifc"
+        url: "./ifc_files/01.ifc"
     },
     {
         name: "Model 2",
         id: "2",
-        url: "ifc_files\\02.ifc"
+        url: "./ifc_files/02.ifc"
     },
     {
         name: "Model 3",
         id: "3",
-        url: "ifc_files\\03.ifc"
+        url: "./ifc_files/03.ifc"
     },
     {
         name: "Model 4",
         id: "4",
-        url: "ifc_files\\04.ifc"
+        url: "./ifc_files/04.ifc"
     },
     {
         name: "Model 5",
         id: "5",
-        url: "ifc_files\\05.ifc"
+        url: "./ifc_files/05.ifc"
     },
    
 ]
